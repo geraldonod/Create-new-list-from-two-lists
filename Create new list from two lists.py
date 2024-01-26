1c34a8e364eff452735089a941b691b1fdceb863
Gerald <geraldonod@gmail.com>
2024-01-26 11:25:01 +0800

    loop for first list

--- Create new list from two lists.py
+++ Create new list from two lists.py
@@ -4,6 +4,10 @@ def merge_list(list1, list2):
     
 #create for loop for first list
     
+    for num in list1:
+        if num % 2 != 0:
+            result_list.append(num)
+    
 #create for loop for second list
 
 #print result
