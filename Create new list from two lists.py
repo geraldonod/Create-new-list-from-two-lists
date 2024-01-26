fde04f40ce7ce11eac6394c2c50043d3656109af
Gerald <geraldonod@gmail.com>
2024-01-26 11:17:49 +0800

    user defined function

--- Create new list from two lists.py
+++ Create new list from two lists.py
@@ -1,7 +1,9 @@
 #create user defined function to combine the lists
-
+def merge_list(list1, list2):
+    result_list = []
+    
 #create for loop for first list
-
+    
 #create for loop for second list
 
 #print result
